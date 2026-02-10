dc3c4b1b18d16c424c6336ee4888739582c65045
defreitasannaclara945-crypto <defreitasannaclara945@gmail.com>
2026-02-10 08:30:51 -0300

    Create questao11exer03.js

---
+++ back_end/questao11exer03.js
@@ -0,0 +1,20 @@
+  contador, numero, soma: inteiro
+
+    Inicio
+        soma <- 0
+
+        Para contador de 1 ate 7 faça
+            Escreva("Digite o ", contador, "º número: ")
+            Leia(numero)
+            soma <- soma + numero
+        FimPara
+
+        Escreva("A soma total é: ", soma)
+Digite o 1º número: 5
+Digite o 2º número: 3
+Digite o 3º número: 10
+Digite o 4º número: 2
+Digite o 5º número: 0
+Digite o 6º número: 4
+Digite o 7º número: 6
+A soma total é: 30
