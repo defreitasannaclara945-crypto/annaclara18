1c9887cc7ffed524669e0d6c1c52286c2c347c34
defreitasannaclara945-crypto <defreitasannaclara945@gmail.com>
2026-02-10 08:29:04 -0300

    Create questao10exer03.js

---
+++ back_end/questao10exer03.js
@@ -0,0 +1,14 @@
+ contador, soma: inteiro
+
+    Inicio
+        soma <- 0
+        contador <- 500
+
+        Enquanto contador >= 0 faça
+            soma <- soma + contador
+            contador <- contador - 50
+        FimEnquanto
+
+        Escreva("O resultado da soma é: ", soma)
+    Fim
+FimAlgoritmoO resultado da soma é: 2750
