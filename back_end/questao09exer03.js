8a7a1241d3bfa2451cb075723429a4391ec3ef47
defreitasannaclara945-crypto <defreitasannaclara945@gmail.com>
2026-02-10 08:27:41 -0300

    Create questao09exer03.js

---
+++ back_end/questao09exer03.js
@@ -0,0 +1,15 @@
+  contador, soma: inteiro
+
+    Inicio
+        soma <- 0
+        contador <- 6
+
+        Enquanto contador <= 100 faça
+            soma <- soma + contador
+            contador <- contador + 2
+        FimEnquanto
+
+        Escreva("A soma é: ", soma)
+    Fim
+FimAlgoritmo
+A soma é: 2550
